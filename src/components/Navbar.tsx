e2d2f24f13b5cfa4509bc089c13bdbc6682cf696
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-12-08 21:56:50 +0100

    Add Navbar component and configure Tailwind CSS with autoprefixer and PostCSS

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+	<>
+	  <nav>
+		<a href="index.html">
+			<img src="" alt="" />
+			<span className="uppercase">mouad el asri</span>
+		</a>
+	  </nav>
+	</>
+  )
+}
+
+export default Navbar;
