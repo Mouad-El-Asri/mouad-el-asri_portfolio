c9940457529498f781eee4196d7c2920d51a36da
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-12-11 00:15:24 +0100

    Refactor project structure: add PostCSS configuration, remove unused utils, update Tailwind CSS setup, and enhance Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,30 @@
+import moelAsrImg from "../assets/moel-asr.png";
+
 const Navbar = () => {
-  return (
-	<>
-	  <nav>
-		<a href="index.html">
-			<img src="" alt="" />
-			<span className="uppercase">mouad el asri</span>
-		</a>
-	  </nav>
-	</>
-  )
-}
+    return (
+        <>
+            <nav>
+                <div className="h-[100px] border-b-[1px] border-b-primary flex justify-between items-center p-8">
+                    <a href="index.html">
+                        <img src={moelAsrImg} alt="moel-asr" />
+                    </a>
+                    <div className="">
+                        <ul className="list-none flex text-lg gap-8">
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Certifications</li>
+                            <li>Portfolio</li>
+                            <li>Blog</li>
+                            <li>Contact</li>
+                        </ul>
+						<div>
+
+						</div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
 
 export default Navbar;
